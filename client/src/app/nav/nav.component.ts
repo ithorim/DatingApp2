188582d85c3c9de84b2aa42cb6cfc3c6e2b25395
Tihomir Rajicic <tihomir.rajicic@gmail.com>
2024-03-13 16:37:28 +0100

    End of Section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -10,9 +12,8 @@ import { User } from '../_models/user';
 })
 export class NavComponent implements OnInit{
   model: any = {}
-  
 
-  constructor (public accountService: AccountService) {}
+  constructor (public accountService: AccountService, private router: Router, private toastr: ToastrService) {}
 
   ngOnInit(): void {
 
@@ -21,15 +22,14 @@ export class NavComponent implements OnInit{
 
   login() { //kad radis sa http requestovima zapravo i ne moras da se unsubscribe, jer se request zavrsava i sub se zavrsava
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
-      error: error => console.log(error)
+      next: _ => this.router.navigateByUrl('/members'), // _ isto sto i () i kaze da ne koristimo argument za ovu metodu koju pozivamo next
+      error: error => this.toastr.error(error.error)
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
